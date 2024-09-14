ce14339bba3fc1b3816c80cf05a8c254460edee3
yuxintao <1921056015@qq.com>
2024-09-14 18:32:30 +0800

    测试tdesign ui组件

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
-        test page
-    </div>
+  <t-button block size="large" variant="outline" theme="primary" @click="visible = true">基础图片预览</t-button>
+  <t-image-viewer v-model:images="images" v-model:visible="visible" />
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const visible = ref(false);
+const images = [
+  'https://tdesign.gtimg.com/mobile/demos/swiper1.png',
+  'https://tdesign.gtimg.com/mobile/demos/swiper2.png',
+];
+</script>

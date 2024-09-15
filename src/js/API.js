868e511c8c1b5806cd76c8d4fa156d336901fa65
yuxintao <1921056015@qq.com>
2024-09-16 07:52:16 +0800

    添加js/API.js

---
+++ src/js/API.js
@@ -0,0 +1,9 @@
+export default {
+    loadDataList: "/file/loadDataList",
+    getFolderInfo: "/file/getFolderInfo",
+    getCover: "/file/getCover",
+    getFile: "/file/getFile",
+    getVideo: "/file/getVideo",
+    createDownloadUrl: "file/createDownloadUrl",
+    download: "/file/download",
+}

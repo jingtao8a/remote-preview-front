d24e1bbef60554249a23642b8825d4df395eef2b
yuxintao <1921056015@qq.com>
2024-09-16 08:38:43 +0800

    加入route（/brief /detail）

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Test from '../views/Test.vue'
 import Main from '../views/Main.vue'
+import Brief from '../views/Brief.vue'
+import Detail from '../views/Detail.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,20 @@ const router = createRouter({
         path: '/',
         name: "main",
         component: Main,
+        children: [
+          {
+            path: "/",
+            redirect: "/detail"
+          },
+          {
+            path: "/detail",
+            component: Detail,
+          },
+          {
+            path: "/brief",
+            component: Brief,
+          }
+        ]
       }
     ]
 })

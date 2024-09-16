d9e283dbce0ac86cf32e7feec8aad7dbef800c9c
yuxintao <1921056015@qq.com>
2024-09-16 11:50:18 +0800

    引入图标css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Router from './router/index.js'
 // 引入组件库的少量全局样式变量
 import 'tdesign-mobile-vue/es/style/index.css'
 import TDesign from 'tdesign-mobile-vue'
+//图标
+import './assets/icon/iconfont.css'
 
 const app = createApp(App)
 
